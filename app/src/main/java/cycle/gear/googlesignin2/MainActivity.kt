7e13111c3e36c3fab3cfa656cb8778223cf993a8
@adityakadlag2004 <adityakadlag2004@gmail.com>
2020-10-28 14:15:18 +0530

    Working on drawer Layout and more

--- app/src/main/java/cycle/gear/googlesignin2/MainActivity.kt
+++ app/src/main/java/cycle/gear/googlesignin2/MainActivity.kt
@@ -1,26 +1,45 @@
 package cycle.gear.googlesignin2
 
+import Utils.toast
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.view.View
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.google.firebase.auth.FirebaseAuth
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
-
+    private lateinit var toggle:ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         auth = FirebaseAuth.getInstance()
+      //  toolbar=findViewById(R.id.toolBarMain)
+        setSupportActionBar(toolBarMain)
         checkUser()
-        findViewById<View>(R.id.logOut).setOnClickListener {
-            auth.signOut()
-            checkUser()
+
+
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toggle= ActionBarDrawerToggle(this,drawerLayoutMain,R.string.open,R.string.close)
+        drawerLayoutMain.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId)
+            {
+              R.id.firebaseDatabase->{toast("Working on Firebase Database ")}
+
+              R.id.fireFCM->{ toast("Working on Firebase FCM ")}
+
+
+            }
+            true
         }
     }
 
@@ -38,5 +57,13 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }, 2000)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item))
+        {
+          return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
