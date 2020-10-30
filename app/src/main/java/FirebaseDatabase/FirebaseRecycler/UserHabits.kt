88c392ebd220e056053aa419e0f6b63df70a4e89
@adityakadlag2004 <adityakadlag2004@gmail.com>
2020-10-30 15:01:27 +0530

    Floating Action Button Added

--- app/src/main/java/FirebaseDatabase/FirebaseRecycler/UserHabits.kt
+++ app/src/main/java/FirebaseDatabase/FirebaseRecycler/UserHabits.kt
@@ -1,9 +1,14 @@
 package FirebaseDatabase.FirebaseRecycler
 
 import FirebaseDatabase.UserHabit
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.INVISIBLE
+import androidx.constraintlayout.widget.ConstraintSet.INVISIBLE
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import cycle.gear.googlesignin2.R
 import kotlinx.android.synthetic.main.activity_user_habits.*
 
@@ -11,11 +16,25 @@ class UserHabits : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_habits)
-        val exampleHabits=dummyTeastHabit(25)
+        val exampleHabits = dummyTeastHabit(10)
 
-        habit_RecyclerView.adapter=HabitsAdapter(exampleHabits)
-        habit_RecyclerView.layoutManager=LinearLayoutManager(this)
+        habit_RecyclerView.adapter = HabitsAdapter(exampleHabits)
+        habit_RecyclerView.layoutManager = LinearLayoutManager(this)
         habit_RecyclerView.setHasFixedSize(true)
+        habit_RecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                if (dy > 0) {
+                    habits_FloatingActionBtn.visibility = View.INVISIBLE
+                } else {
+                    habits_FloatingActionBtn.visibility = View.VISIBLE
+                }
+                super.onScrolled(recyclerView, dx, dy)
+
+            }
+        })
+        habits_FloatingActionBtn.setOnClickListener {
+
+        }
     }
 
     private fun dummyTeastHabit(size: Int): List<UserHabit> {
