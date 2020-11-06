b6f27f63585648c05ee1b11642d87e9527f3f0f2
@adityakadlag2004 <adityakadlag2004@gmail.com>
2020-11-06 20:17:42 +0530

    recyclerView Decorated little bit

--- app/src/main/java/firebasedatabase/firebaserecycler/UserHabits.kt
+++ app/src/main/java/firebasedatabase/firebaserecycler/UserHabits.kt
@@ -11,6 +11,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
@@ -70,6 +71,10 @@ class UserHabits : AppCompatActivity() {
         habit_RecyclerView.adapter = adapter
         habit_RecyclerView.layoutManager = LinearLayoutManager(this)
         habit_RecyclerView.setHasFixedSize(true)
+        habit_RecyclerView.addItemDecoration(
+            DividerItemDecoration(this, LinearLayoutManager.VERTICAL)
+        )
+
     }
 
     private fun openDialog() {
